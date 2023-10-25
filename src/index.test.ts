ea7994b54ad162f1bbe598be293d706989281d2c
Shuntaro Nishizawa <me@s2n.tech>
2023-10-26 00:03:37 +0900

    feat: Add transform()

---
+++ src/index.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from "bun:test";
+import { transform } from ".";
+import { Root } from "mdast";
+
+describe("transform()", () => {
+  test("basic", () => {
+    const content = "Hello, world!";
+
+    const actural = transform(content);
+    const expected = {
+      type: "root",
+      children: [
+        {
+          type: "paragraph",
+          children: [
+            {
+              type: "text",
+              value: "Hello, world!",
+              position: {
+                start: { line: 1, column: 1, offset: 0 },
+                end: { line: 1, column: 14, offset: 13 },
+              },
+            },
+          ],
+          position: {
+            start: { line: 1, column: 1, offset: 0 },
+            end: { line: 1, column: 14, offset: 13 },
+          },
+        },
+      ],
+      position: {
+        start: { line: 1, column: 1, offset: 0 },
+        end: { line: 1, column: 14, offset: 13 },
+      },
+    } satisfies Root;
+
+    expect(actural).toEqual(expected);
+  });
+});

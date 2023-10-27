9c08e1ae740e8298c34f0a1b8fafcbd94d32c60a
Shuntaro Nishizawa <me@s2n.tech>
2023-10-28 00:55:59 +0900

    feat: Use partial gfm syntax

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import type { Root } from "mdast";
 import { fromMarkdown } from "mdast-util-from-markdown";
-import { gfm } from "micromark-extension-gfm";
+import { gfmAutolinkLiteral } from "micromark-extension-gfm-autolink-literal";
+import { gfmFootnote } from "micromark-extension-gfm-footnote";
+import { gfmStrikethrough } from "micromark-extension-gfm-strikethrough";
+import { gfmTable } from "micromark-extension-gfm-table";
 import { math } from "micromark-extension-math";
 
 import { descriptionList } from "./extensions/description-list.js";
@@ -8,7 +11,13 @@ import { resolveReference } from "./extensions/resolve-reference.js";
 
 export function transform(content: string): Root {
   return fromMarkdown(content, {
-    extensions: [gfm(), math()],
+    extensions: [
+      gfmAutolinkLiteral(),
+      gfmFootnote(),
+      gfmStrikethrough(),
+      gfmTable(),
+      math(),
+    ],
     mdastExtensions: [resolveReference(), descriptionList()],
   });
 }

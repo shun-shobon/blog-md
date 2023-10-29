45bce5eea7e33fa0816b2a4fdca3ed401e7b7aec
Shuntaro Nishizawa <me@s2n.tech>
2023-10-30 01:58:43 +0900

    feat(transform): Add description list extension

---
+++ src/transform/extensions/description-list.ts
@@ -0,0 +1,65 @@
+import type * as AST from "../../ast.js";
+import type {
+  DescriptionDetails,
+  DescriptionList,
+  DescriptionTerm,
+} from "../../parse/extensions/description-list.js";
+import type { Extension, MdastHandler } from "../extension.js";
+
+export function descriptionList(): Extension {
+  return {
+    handlers: {
+      descriptionList: descriptionListNode,
+      descriptionTerm,
+      descriptionDetails,
+    },
+  };
+}
+
+const descriptionListNode: MdastHandler<DescriptionList> = (
+  node,
+  handleChildren,
+) => {
+  const children = handleChildren(node);
+  if (children.length === 0) return;
+
+  const newNode: AST.DescriptionList = {
+    type: "descriptionList",
+    children,
+    position: node.position,
+  };
+
+  return newNode;
+};
+
+const descriptionTerm: MdastHandler<DescriptionTerm> = (
+  node,
+  handleChildren,
+) => {
+  const children = handleChildren(node);
+  if (children.length === 0) return;
+
+  const newNode: AST.DescriptionTerm = {
+    type: "descriptionTerm",
+    children,
+    position: node.position,
+  };
+
+  return newNode;
+};
+
+const descriptionDetails: MdastHandler<DescriptionDetails> = (
+  node,
+  handleChildren,
+) => {
+  const children = handleChildren(node);
+  if (children.length === 0) return;
+
+  const newNode: AST.DescriptionDetails = {
+    type: "descriptionDetails",
+    children,
+    position: node.position,
+  };
+
+  return newNode;
+};

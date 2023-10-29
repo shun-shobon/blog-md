4cdfe9f6d4206e81875bd23a9312564f0bdae897
Shuntaro Nishizawa <me@s2n.tech>
2023-10-30 01:59:22 +0900

    feat(transform): Add some extension

---
+++ src/transform/extensions/embed.ts
@@ -0,0 +1,21 @@
+import type * as AST from "../../ast.js";
+import type { Embed } from "../../parse/extensions/embed.js";
+import type { Extension, MdastHandler } from "../extension.js";
+
+export function embed(): Extension {
+  return {
+    handlers: {
+      embed: embedNode,
+    },
+  };
+}
+
+const embedNode: MdastHandler<Embed> = (node) => {
+  const newNode: AST.Embed = {
+    type: "embed",
+    value: node.value,
+    position: node.position,
+  };
+
+  return newNode;
+};

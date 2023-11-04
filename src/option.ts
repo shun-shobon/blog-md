b645eaea07fd1732755b99402aaee6013df674eb
Shuntaro Nishizawa <me@s2n.tech>
2023-11-05 03:12:20 +0900

    refactor: Delete option

--- src/option.ts
+++
@@ -1,9 +0,0 @@
-type Option<T> = T | undefined;
-
-export function map<T, U>(fn: (x: T) => U): (x: Option<T>) => Option<U> {
-  return (x) => (x === undefined ? undefined : fn(x));
-}
-
-export function or<T>(y: Option<T>): (x: Option<T>) => Option<T> {
-  return (x) => (x === undefined ? y : x);
-}
